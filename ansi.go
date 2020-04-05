5666466d57c1890e831e8cf403dcbaa424e755d9
Aidan Oldershaw <aoldershaw@pivotal.io>
2020-04-05 18:40:30 -0400

    add state management
    The top-level ansi package is responsible for consuming the actions
    generated by the parser and modifying some internal state. Any
    interesting work related to printing is delegated to a `Printer`
    interface (which can Print bytes with certain styles applied, as well as
    Clear bytes to the right of a position on a line).
    
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- ansi.go
+++ ansi.go
@@ -1,4 +1,129 @@
 package ansi
 
+import (
+	"bytes"
+	"github.com/aoldershaw/ansi/action"
+)
+
+type Printer interface {
+	Print(data []byte, style Style, pos action.Pos)
+	ClearRight(pos action.Pos)
+}
+
 type State struct {
+	Style          Style
+	LineDiscipline LineDiscipline
+	Position       action.Pos
+	SavedPosition  *action.Pos
+
+	printer Printer
+}
+
+func New(lineDiscipline LineDiscipline, printer Printer) *State {
+	return &State{
+		LineDiscipline: lineDiscipline,
+		Style:          Style{},
+
+		printer: printer,
+	}
+}
+
+type Style struct {
+	Foreground action.Color `json:"fg,omitempty"`
+	Background action.Color `json:"bg,omitempty"`
+	Bold       bool         `json:"bold,omitempty"`
+	Faint      bool         `json:"faint,omitempty"`
+	Italic     bool         `json:"italic,omitempty"`
+	Underline  bool         `json:"underline,omitempty"`
+	Blink      bool         `json:"blink,omitempty"`
+	Inverted   bool         `json:"inverted,omitempty"`
+	Fraktur    bool         `json:"fraktur,omitempty"`
+	Framed     bool         `json:"framed,omitempty"`
+}
+
+type LineDiscipline int
+
+const (
+	Raw LineDiscipline = iota
+	Cooked
+)
+
+func (s *State) Action(act action.Action) {
+	switch v := act.(type) {
+	case action.Print:
+		s.printer.Print(v, s.Style, s.Position)
+		s.moveCursor(0, len(v))
+	case action.Reset:
+		s.Style = Style{}
+	case action.SetForeground:
+		s.Style.Foreground = action.Color(v)
+	case action.SetBackground:
+		s.Style.Background = action.Color(v)
+	case action.SetBold:
+		s.Style.Bold = bool(v)
+	case action.SetFaint:
+		s.Style.Faint = bool(v)
+	case action.SetItalic:
+		s.Style.Italic = bool(v)
+	case action.SetUnderline:
+		s.Style.Underline = bool(v)
+	case action.SetBlink:
+		s.Style.Blink = bool(v)
+	case action.SetInverted:
+		s.Style.Inverted = bool(v)
+	case action.SetFraktur:
+		s.Style.Fraktur = bool(v)
+	case action.SetFramed:
+		s.Style.Framed = bool(v)
+	case action.CursorPosition:
+		s.Position = action.Pos(v)
+	case action.CursorUp:
+		s.moveCursor(-int(v), 0)
+	case action.CursorDown:
+		s.moveCursor(int(v), 0)
+	case action.CursorForward:
+		s.moveCursor(0, int(v))
+	case action.CursorBack:
+		s.moveCursor(0, -int(v))
+	case action.CursorColumn:
+		s.Position.Col = int(v)
+	case action.SaveCursorPosition:
+		pos := s.Position
+		s.SavedPosition = &pos
+	case action.RestoreCursorPosition:
+		if s.SavedPosition != nil {
+			s.Position = *s.SavedPosition
+		}
+	case action.EraseLine:
+		startOfLine := s.Position
+		startOfLine.Col = 0
+		switch action.EraseMode(v) {
+		case action.EraseToBeginning:
+			if s.Position.Col == 0 {
+				return
+			}
+			empty := bytes.Repeat([]byte{' '}, s.Position.Col)
+			s.printer.Print(empty, Style{}, startOfLine)
+		case action.EraseToEnd:
+			pos := s.Position
+			pos.Col++
+			s.printer.ClearRight(pos)
+		case action.EraseAll:
+			s.printer.ClearRight(startOfLine)
+		}
+
+	case action.EraseDisplay:
+		// unsupported
+	}
+}
+
+func (s *State) moveCursor(r, c int) {
+	s.Position.Line += r
+	s.Position.Col += c
+	if s.Position.Line < 0 {
+		s.Position.Line = 0
+	}
+	if s.Position.Col < 0 {
+		s.Position.Col = 0
+	}
 }
--- ansi_test.go
+++ ansi_test.go
@@ -1,1 +1,265 @@
-package ansi
+package ansi_test
+
+import (
+	"bytes"
+	"github.com/aoldershaw/ansi"
+	"github.com/aoldershaw/ansi/action"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+type printCall struct {
+	data  []byte
+	style ansi.Style
+	pos   action.Pos
+}
+
+type clearCall struct {
+	pos action.Pos
+}
+
+type spyPrinter struct {
+	printCalls []printCall
+	clearCalls []clearCall
+}
+
+func (p *spyPrinter) Print(data []byte, style ansi.Style, pos action.Pos) {
+	p.printCalls = append(p.printCalls, printCall{
+		data:  data,
+		style: style,
+		pos:   pos,
+	})
+}
+
+func (p *spyPrinter) ClearRight(pos action.Pos) {
+	p.clearCalls = append(p.clearCalls, clearCall{
+		pos: pos,
+	})
+}
+
+func TestAnsi(t *testing.T) {
+	for _, tt := range []struct {
+		description    string
+		lineDiscipline ansi.LineDiscipline
+		actions        []action.Action
+		printCalls     []printCall
+		clearCalls     []clearCall
+	}{
+		{
+			description: "makes print calls",
+			actions: []action.Action{
+				action.Print("some bytes here"),
+			},
+			printCalls: []printCall{
+				{
+					data: []byte("some bytes here"),
+				},
+			},
+		},
+		{
+			description: "applies styles",
+			actions: []action.Action{
+				action.SetForeground(action.Red),
+				action.SetBackground(action.Blue),
+				action.SetBold(true),
+				action.SetFraktur(true),
+				action.SetUnderline(true),
+				action.SetItalic(true),
+				action.SetInverted(true),
+				action.SetFaint(true),
+				action.SetBlink(true),
+				action.SetFramed(true),
+				action.Print("some nicely formatted bytes here"),
+			},
+			printCalls: []printCall{
+				{
+					data: []byte("some nicely formatted bytes here"),
+					style: ansi.Style{
+						Foreground: action.Red,
+						Background: action.Blue,
+						Bold:       true,
+						Faint:      true,
+						Italic:     true,
+						Underline:  true,
+						Blink:      true,
+						Inverted:   true,
+						Fraktur:    true,
+						Framed:     true,
+					},
+				},
+			},
+		},
+		{
+			description: "resets styles",
+			actions: []action.Action{
+				action.SetForeground(action.Red),
+				action.SetBold(true),
+				action.Reset{},
+				action.Print("some unformatted bytes here"),
+			},
+			printCalls: []printCall{
+				{
+					data:  []byte("some unformatted bytes here"),
+					style: ansi.Style{},
+				},
+			},
+		},
+		{
+			description: "print calls move cursor",
+			actions: []action.Action{
+				action.Print("abc"),
+				action.Print("123"),
+			},
+			printCalls: []printCall{
+				{
+					data: []byte("abc"),
+					pos:  action.Pos{Line: 0, Col: 0},
+				},
+				{
+					data: []byte("123"),
+					pos:  action.Pos{Line: 0, Col: 3},
+				},
+			},
+		},
+		{
+			description: "can move cursor",
+			actions: []action.Action{
+				action.Print("(0,0)"),
+				action.CursorDown(2),
+				action.Print("(2,5)"),
+				action.CursorUp(1),
+				action.CursorColumn(15),
+				action.Print("(1,15)"),
+				action.CursorPosition{Line: 4, Col: 20},
+				action.Print("(4,20)"),
+				action.CursorColumn(10),
+				action.CursorForward(5),
+				action.Print("(4,15)"),
+				action.CursorColumn(10),
+				action.CursorBack(5),
+				action.Print("(4,5)"),
+				action.CursorBack(10000),
+				action.Print("(4,0)"),
+			},
+			printCalls: []printCall{
+				{
+					data: []byte("(0,0)"),
+					pos:  action.Pos{Line: 0, Col: 0},
+				},
+				{
+					data: []byte("(2,5)"),
+					pos:  action.Pos{Line: 2, Col: 5},
+				},
+				{
+					data: []byte("(1,15)"),
+					pos:  action.Pos{Line: 1, Col: 15},
+				},
+				{
+					data: []byte("(4,20)"),
+					pos:  action.Pos{Line: 4, Col: 20},
+				},
+				{
+					data: []byte("(4,15)"),
+					pos:  action.Pos{Line: 4, Col: 15},
+				},
+				{
+					data: []byte("(4,5)"),
+					pos:  action.Pos{Line: 4, Col: 5},
+				},
+				{
+					data: []byte("(4,0)"),
+					pos:  action.Pos{Line: 4, Col: 0},
+				},
+			},
+		},
+		{
+			description: "can save/restore cursor position",
+			actions: []action.Action{
+				action.CursorPosition{Line: 123, Col: 456},
+				action.SaveCursorPosition{},
+				action.CursorPosition{Line: 0, Col: 0},
+				action.RestoreCursorPosition{},
+				action.Print("i'm back!"),
+			},
+			printCalls: []printCall{
+				{
+					data: []byte("i'm back!"),
+					pos:  action.Pos{Line: 123, Col: 456},
+				},
+			},
+		},
+		{
+			description: "restoring position when nothing is saved does nothing",
+			actions: []action.Action{
+				action.CursorPosition{Line: 1, Col: 2},
+				action.RestoreCursorPosition{},
+				action.Print("no change"),
+			},
+			printCalls: []printCall{
+				{
+					data: []byte("no change"),
+					pos:  action.Pos{Line: 1, Col: 2},
+				},
+			},
+		},
+		{
+			description: "restoring position when nothing is saved does nothing",
+			actions: []action.Action{
+				action.CursorPosition{Line: 1, Col: 2},
+				action.RestoreCursorPosition{},
+				action.Print("no change"),
+			},
+			printCalls: []printCall{
+				{
+					data: []byte("no change"),
+					pos:  action.Pos{Line: 1, Col: 2},
+				},
+			},
+		},
+		{
+			description: "erasing lines",
+			actions: []action.Action{
+				action.Print("some bytes"),
+				action.EraseLine(action.EraseToBeginning),
+				action.EraseLine(action.EraseToEnd),
+				action.EraseLine(action.EraseAll),
+				action.Print("some more bytes"),
+			},
+			printCalls: []printCall{
+				{
+					data: []byte("some bytes"),
+					pos:  action.Pos{Line: 0, Col: 0},
+				},
+				{
+					data: bytes.Repeat([]byte{' '}, 10),
+					pos:  action.Pos{Line: 0, Col: 0},
+				},
+				{
+					data: []byte("some more bytes"),
+					pos:  action.Pos{Line: 0, Col: 10},
+				},
+			},
+			clearCalls: []clearCall{
+				{
+					pos: action.Pos{Line: 0, Col: 11},
+				},
+				{
+					pos: action.Pos{Line: 0, Col: 0},
+				},
+			},
+		},
+	} {
+		t.Run(tt.description, func(t *testing.T) {
+			g := NewGomegaWithT(t)
+			spyPrinter := &spyPrinter{}
+			state := ansi.New(tt.lineDiscipline, spyPrinter)
+
+			for _, act := range tt.actions {
+				state.Action(act)
+			}
+
+			g.Expect(spyPrinter.printCalls).To(Equal(tt.printCalls))
+			g.Expect(spyPrinter.clearCalls).To(Equal(tt.clearCalls))
+		})
+	}
+}
